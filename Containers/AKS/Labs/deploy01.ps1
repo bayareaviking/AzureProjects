26e1c522e295f2db08122c51184969f292de894a
Marcus Larsson <bayareaviking@hotmail.com>
2024-10-09 19:14:30 -0700

    Updating deployment yaml file

--- Containers/AKS/Labs/deploy01.ps1
+++ Containers/AKS/Labs/deploy01.ps1
@@ -1,4 +1,8 @@
 $tenantID = "027ae224-51cb-4c9a-b8c8-be9b6d46383b"
 $subID = "d70a6468-b750-4907-a466-f89146780348"
+$CLUSTER_NAME = "aks-contoso-video"
+$RESOURCE_GROUP = "rg-contoso-video"
 
-az login --subscription $subID --tenant $tenantID
+az login --tenant $tenantID #--subscription $subID
+
+az aks get-credentials --name $CLUSTER_NAME --resource-group $RESOURCE_GROUP
